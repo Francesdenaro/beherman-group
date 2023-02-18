ddb140378e7c2a5cca27a24e647709083a2a5fd4
Francesco <fdenaro96@gmail.com>
2023-02-18 14:53:31 +0100

    Fixed SVGs syntax

--- src/comps/Svg/Burger.tsx
+++ src/comps/Svg/Burger.tsx
@@ -1,9 +1,9 @@
 const Burger = () => {
 	return (
 		<svg width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M0 1L20 1" stroke="white" stroke-width="1.5" />
-			<path d="M0 6L20 6" stroke="white" stroke-width="1.5" />
-			<path d="M0 11L20 11" stroke="white" stroke-width="1.5" />
+			<path d="M0 1L20 1" stroke="white" strokeWidth="1.5" />
+			<path d="M0 6L20 6" stroke="white" strokeWidth="1.5" />
+			<path d="M0 11L20 11" stroke="white" strokeWidth="1.5" />
 		</svg>
 	);
 };
